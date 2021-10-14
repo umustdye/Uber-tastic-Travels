b5b7f082fee0039ab8e78de25f0c0d5638443e82
ianCSMajor <brucewinter123@gmail.com>
2021-10-14 11:52:09 -0700

    Basic CRUD API to add/delete members

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Ont',
+        email: 'jont@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Carrie Lad',
+        email: 'clad@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
