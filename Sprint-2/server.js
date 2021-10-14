6edceb9cb92694f78fb0ee91dc5289346d591579
umustdye <43763861+umustdye@users.noreply.github.com>
2021-10-14 01:13:24 -0700

    client server communication with dataset

---
+++ Sprint-2/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const UberParser = require('./uber_parser.js');
+var bodyParser = require('body-parser');
+const { application } = require('express');
+
+const server = express();
+
+const port = 3000;
+
+server.listen(port, function(error) {
+    if(error) {
+        console.log("Something went wrong", error)
+    } else {
+        console.log("Server is listening on port " + port)
+    }
+})
+
+//console.log(UberParser.ParseUber());
+
+uber = UberParser.ParseUber();
+
+//public is name of html directory, basically website shtuff
+server.use(express.static('public'));
+
+server.use(bodyParser.json());
+
+server.get('/pickup_date', function(req, res){
+    res.send({ uber_date: uber});
+});
+
