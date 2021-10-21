e52f066697f0a27ac7dd33ecbcf03cb414f4aeb1
umustdye <43763861+umustdye@users.noreply.github.com>
2021-10-21 11:50:33 -0700

    comparing different ride services

--- Sprint-2/server.js
+++ Sprint-2/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const UberParser = require('./uber_parser.js');
+const fhvParser = require('./fhv_parser.js');
+const Noah = require('./Noah_Parse.js');
+const cab_rides_parser = require('./cab_type_parse.js');
 var bodyParser = require('body-parser');
 const { application } = require('express');
 
@@ -17,7 +20,14 @@ server.listen(port, function(error) {
 
 //console.log(UberParser.ParseUber());
 
+//datasets
 uber = UberParser.ParseUber();
+fhv = fhvParser.Parsefhv();
+cab_rides = cab_rides_parser.ParseCab_Rides();
+
+//console.log(Parser.CompareBasedOnMonth());
+compareDiplo = Noah.CompareBasedOnMonth(uber, fhv, "Diplo");
+
 
 //public is name of html directory, basically website shtuff
 server.use(express.static('public'));
@@ -25,6 +35,43 @@ server.use(express.static('public'));
 server.use(bodyParser.json());
 
 server.get('/pickup_date', function(req, res){
-    res.send({ uber_date: uber});
+    res.send({ uber_date: uber.slice(1, 20)});
+});
+
+server.get('/compareDiplo', function(req, res){
+    res.send({comparing: compareDiplo});
+});
+
+server.post('/search_results', function(req, res) {
+    var results = [];
+    var currLoc = req.body.address;
+    console.log(req.body.rideService, req.body.dateBegin, req.body.dateEnd, req.body.timeBegin, req.body.timeEnd, req.body.address);
+    if (currLoc.length == 0) {
+        currLoc = ' | ';
+    }
+    results = Noah.SearchByParameter(uber, fhv, req.body.rideService, req.body.dateBegin, req.body.dateEnd, req.body.timeBegin, req.body.timeEnd, currLoc);
+    console.log('Great Success!!');
+    // console.log(results);
+    if (results.length == 0) {
+        results = [{"Date": 'No Data Available', "Time": 'No Data Available', "Address": 'No Data Available'}, {"Date": 'No Data Available', "Time": 'No Data Available', "Address": 'No Data Available'}];
+    }
+    // console.log(results);
+    res.send(results);
+});
+
+
+server.post('/compare_results', function(req, res) {
+    var results = [];
+
+    console.log(req.body.rideService);
+
+    results = Noah.CompareBasedOnMonth(uber, fhv, req.body.rideService);
+    console.log('Great Success!!');
+    // console.log(results);
+    if (results.length == 0) {
+        results = [{"Month": 'No Data Available', "uber_rides": 'No Data Available', "f_rides": 'No Data Available'}];
+    }
+    // console.log(results);
+    res.send({comparing: results});
 });
 
